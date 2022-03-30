cdaeb8cfad1b0052858ed1a5384b53ec95fb26f1
enesakoluk <enes.akoluk@gmail.com>
2022-03-30 17:37:28 +0300

    +

--- user/models.py
+++ user/models.py
@@ -5,6 +5,8 @@ class Profile(models.Model):
     following = models.ManyToManyField(User, blank=True, related_name="followers", symmetrical=False)
     bio = models.TextField(max_length=200, blank=True, default="Bio")
     website = models.URLField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,blank=True,null=True)
     # def __str__(self):
     #     return self.user
     def __str__(self):
