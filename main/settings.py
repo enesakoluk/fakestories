d679ce75f20471e0cc79b392157967b1f16f0785
enesakoluk <enes.akoluk@gmail.com>
2022-04-02 22:05:22 +0300

    aa

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kn_4*44s9fnkj@jk-7=r_mfs(ahfcv0ngjpm*j=qjifdz+tju1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
